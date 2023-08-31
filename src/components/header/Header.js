5fc57b9a2ba9e66f57b9f4051da4bc15f21abf7b
Maximilian <code@koby.dev>
2023-08-31 09:19:47 +0200

    [TASK] Testing Background-Image Header Element
    * Added test element for background-image header (WIP)
    * Added styles for background-image
    * Added testing image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import HeaderAnimation from './HeaderAnimation';
 import Headline from "@/components/headline-container/HeadlineContainer";
 import Section from "@/layouts/Section";
+import Image from 'next/image';
 
 export default function Header({ logo, title, size }) {
   return (
-    <header className={`header size-${size}`}>
+    <header className={`header size-${size} has-animation`}>
       <Section spacingBottom={true} spacingTop={false}> 
         <Headline headline={title} subline="Kobys world" tag="h1" />
       </Section>
@@ -14,3 +15,10 @@ export default function Header({ logo, title, size }) {
     </header>
   );
 };
+
+/*
+  '.has-background-image'
+  <div className="background-image">
+    <Image src="/background-pattern.jpg" alt="Header background image" width="1920" height="1080" objectFit="cover" quality={100} />
+  </div>
+*/
